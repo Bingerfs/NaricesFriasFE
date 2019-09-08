3539cb3a2ad8d114892d4ff154a72af2ad7e50eb
Arturo <art_ag7@hotmail.com>
2019-09-08 04:10:59 -0400

    Primera implementacion de adoptados

--- src/app/adoptados/adoptados.component.ts
+++ src/app/adoptados/adoptados.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ADOPTADOS} from '../mock-adoptados'
+import { Adoptado } from '../adoptado';
 
 @Component({
   selector: 'app-adoptados',
   templateUrl: './adoptados.component.html',
   styleUrls: ['./adoptados.component.css']
 })
-export class AdoptadosComponent implements OnInit {
+export class AdoptadosComponent implements OnInit
+{
+  adoptados = ADOPTADOS;
+
+  /*
+  selectedAdoptado: Adoptado;
+  onSelect(adoptado: Adoptado): void
+  {
+  this.selectedAdoptado = adoptado;
+  }*/
 
   constructor() { }
 
