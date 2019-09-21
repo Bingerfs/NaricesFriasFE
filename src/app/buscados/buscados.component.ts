7a73960593973dbd07546dcfc0f97bab9b8a1d33
ariel <ariel_arc.95@hotmail.com>
2019-09-21 12:20:02 -0400

    Se añadio lista de perros buscados con imagenes

--- src/app/buscados/buscados.component.ts
+++ src/app/buscados/buscados.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Buscado} from './buscado'
-
+import {BUSCADOS} from '../mock-buscados'
 @Component({
   selector: 'app-buscados',
   templateUrl: './buscados.component.html',
@@ -8,54 +8,7 @@ import {Buscado} from './buscado'
 })
 export class BuscadosComponent implements OnInit {
 
-  mascota1 : Buscado = {
-      id: 1,
-      name: 'Cachuchin',
-      size: 'Pequeño',
-      age: 'Cachorro',
-      contacto: 79775899,
-      esterelizacion: true
-  };
-  mascota2 : Buscado = {
-    id: 1,
-    name: 'Kiko',
-    size: 'Pequeño',
-    age: 'Joven',
-    contacto: 79775866,
-    esterelizacion: true
-};
-mascota3 : Buscado = {
-  id: 1,
-  name: 'Firulais',
-  size: 'Grande',
-  age: 'adulto',
-  contacto: 75896322,
-  esterelizacion: true
-};
-mascota4 : Buscado = {
-  id: 1,
-  name: 'Shaggy',
-  size: 'Pequeño',
-  age: 'Cachorro',
-  contacto: 74589633,
-  esterelizacion: true
-};
-mascota5 : Buscado = {
-  id: 1,
-  name: 'Peggy',
-  size: 'Mediano',
-  age: 'Cachorro',
-  contacto: 79458622,
-  esterelizacion: true
-};
-mascota6 : Buscado = {
-  id: 1,
-  name: 'Lia',
-  size: 'Pequeño',
-  age: 'Cachorro',
-  contacto: 74536955,
-  esterelizacion: true
-};
+  buscados = BUSCADOS;
   constructor() { }
 
   ngOnInit() {
