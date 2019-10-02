7d6165985f399b1c664a0f4e62e87161f9ff88e0
Hector Hidalgo <marce.753@gmail.com>
2019-10-02 13:18:25 -0400

    Idek

--- src/app/buscados/buscados.component.ts
+++ src/app/buscados/buscados.component.ts
@@ -13,15 +13,11 @@ export class BuscadosComponent implements OnInit {
   //buscados = BUSCADOS;
   buscados: Buscado[];
   selectedBuscado: Buscado;
-  n = 4;
-  matrix = Array
-  .from({ length: Math.ceil(this.buscados.length / this.n) }, (_, i) => i)
-  .map(i => this.buscados.slice(i * this.n, i * this.n + this.n));
+ 
   constructor(private buscadoService: BuscadoService) { }
 
   ngOnInit() {
     this.getBuscados();
-    const a = 4;
   }
 
   onSelect(buscado: Buscado): void{
