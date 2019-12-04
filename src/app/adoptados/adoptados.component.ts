cf6e495999919cffda0c9689985f6f4e18757e49
matblanco <mateoblaurq@gmail.com>
2019-12-04 12:20:50 -0400

    filtros

--- src/app/adoptados/adoptados.component.ts
+++ src/app/adoptados/adoptados.component.ts
@@ -3,7 +3,8 @@ import { AngularTokenService } from 'angular-token';
 
 import { Adoptado } from '../adoptado';
 import { ApiService } from '../api.service';
-
+import { RouterModule, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-adoptados',
   templateUrl: './adoptados.component.html',
@@ -15,7 +16,7 @@ export class AdoptadosComponent implements OnInit
   public adoptados: Array<Adoptado>
   public selectedAdoptado: Adoptado;
   public imgURL: string = './assets/images/DogProfile.png';
-  constructor(public apiService:ApiService, private tokenService: AngularTokenService) { }
+  constructor(public apiService:ApiService, private tokenService: AngularTokenService,  private router:Router) { }
 
   ngOnInit() {
     this.apiService.get("adoptados").subscribe((data : Adoptado[])=>{
@@ -30,4 +31,32 @@ export class AdoptadosComponent implements OnInit
   onSelect(adoptado: Adoptado): void{
     this.selectedAdoptado = adoptado;
   }
+
+  filtrarTam(value:any){
+    // console.log("tam:");
+    // console.log(value);
+    
+
+    this.adoptados = this.adoptados.filter(h => h.tamagno == value);
+    console.log(this.adoptados);
+    this.router.navigateByUrl('/adoptados');
+    // this.apiService.get("adoptados").subscribe((data : Adoptado[])=>{
+    // this.adoptados=data;
+    // });
+   
+  }
+
+  filtrarGen(value:any){
+    this.adoptados = this.adoptados.filter(h => h.genero == value);
+    
+    console.log(this.adoptados);
+  }
+
+  filtrarEdad(value:any){
+    this.adoptados = this.adoptados.filter(h => h.edad == value);
+    
+    console.log(this.adoptados);
+  }
+
+
 }
