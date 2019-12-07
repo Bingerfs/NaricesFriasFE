efd3ce9fa3f9fd56455ee8fad9d8a2e9ce272319
ariel <ariel_arc.95@hotmail.com>
2019-12-07 15:13:07 -0400

    Se consume las imagenes del backend

--- src/app/adoptados/adoptados.component.ts
+++ src/app/adoptados/adoptados.component.ts
@@ -12,7 +12,9 @@ import { ApiService } from '../api.service';
 
 export class AdoptadosComponent implements OnInit
 {
-  public adoptados: Array<Adoptado>
+  public adoptados: Array<Adoptado>;
+  public filtrados: Array<Adoptado>;
+  public adoptadosBackup: Array<Adoptado>;
   public selectedAdoptado: Adoptado;
   public imgURL: string = './assets/images/DogProfile.png';
   constructor(public apiService:ApiService, private tokenService: AngularTokenService) { }
@@ -22,10 +24,41 @@ export class AdoptadosComponent implements OnInit
       console.log("Datos:");
       console.log(data);
       this.adoptados=data;
+      this.filtrados=data;
+      this.adoptadosBackup = data;
       console.log(this.adoptados);
     });
     
   }
+  filterTam(value:string)
+  {
+    if(value!='none')
+    {
+      this.adoptados = this.filtrados.filter(f => f.tamagno ==value);
+    }
+    else{
+      this.adoptados = this.filtrados;
+    }
+  }
+  filterGen(value:string)
+  {
+    if(value!='none'){
+      this.adoptados = this.filtrados.filter(f => f.genero ==value);
+    }
+    else{
+      this.adoptados = this.filtrados;
+    }
+  }
+  filterAge(value:string)
+  {
+    if(value!='none'){
+      this.adoptados = this.filtrados.filter(f => f.edad ==value);
+    }
+    else{
+      this.adoptados = this.filtrados;
+    }
+  }
+  
 
   onSelect(adoptado: Adoptado): void{
     this.selectedAdoptado = adoptado;
